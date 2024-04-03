9d5dde56c96f4b9341bb54d9743c29bb2eef7df2
Rogério Soares <rogerio_si@yahoo.com.br>
2024-04-03 18:03:12 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,14 +1,16 @@
 from users import Users
 from uavs import Uavs
 from calcchannel import calculate_channel
+
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 #define quantidade de uavbs
 uav = 3
-s = 200
+s = 10
 
 # define qtd de resource blocks
 resBlo = 100
@@ -18,19 +20,20 @@ areay = 1000
 # 01 Creating usuarios de forma randomica
 # Atribuindo os valores para cada usuario: id,x,y, data rate, estacao,qos, snr, etc
 alluser = []
-T = 40  # t = 400 * 1024;  # Taxa Requerida 400 Kbps
+t = 40
 for i in range(s):
-    us = Users(i, random.randint(1, areax), random.randint(1, areay), 1, 1, 0, T, 0, 0, 0, False, 0, 0, 1, 0, 0, 0)
+    taxa = random.randint(35, 40) # t = 400 * 1024;  # Taxa Requerida 400 Kbps
+    us = Users(i, random.randint(1, areax), random.randint(1, areay), taxa, 1, 0, t, 0, 0, 0, False, 0, 0, 1, 0, 0, 0)
     alluser.append(us)
 
 # 03 Creating uavbs em posicoes fixas
 #Atribuindo os valores  cada uavbs: id, x,y,potencia,freq,banda,total,usuarios conect, estacao,totalprb, etc
 alluav = []
-posx = [140, 360, 280]
-posy = [130, 350, 230]
-h=300
+posx = [540, 660, 780]
+posy = [530, 650, 730]
+h=100 #valor multiplicado por 10
 for i in range(uav):
-    uavs = Uavs(i, posx[i], posy[i], 23, 2.4, 20, 50, 30, True, 100, resBlo, False, "False", "implantação", h*2, h, 10, 5, 0) #estrategia pos e alt fixa sem kmeans
+    uavs = Uavs(i, posx[i], posy[i], 23, 2.4, 18, 50, 30, True, 100, resBlo, False, "False", "implantação", h*2, h, 10, 5, 0) #estrategia pos e alt fixa sem kmeans
     alluav.append(uavs)
 
 # 04 alocar usuarios a cada uavbs
@@ -43,7 +46,9 @@ for i in alluser:
     for x in alluav:
         if calculate_channel(i, x, alluav) != (0, 0, 0, 0, 0):
             chanel = calculate_channel(i, x, alluav)
+            #if x.PRB_F >= i.PRB and (chanel[2] > 20 and chanel[2] <50 ) and i.C == False:
             if x.PRB_F >= i.PRB and i.C == False:
+
                 i.EB = x.ID  # estacao base alocada
                 i.ES = 1  # 1 small 2 macro
                 i.CQI = chanel[1]
@@ -55,18 +60,45 @@ for i in alluser:
                 x.U = x.U + 1
                 on = on + 1
                 x.MAX_U += 1
+                i.DR=chanel[0]
 
 #estatistica de alocacao
+mediaSIN=0
+mediapath=0
+nub=0
 for i in alluser:
     print("Status: ", i.C, "ID:", i.ID, i.X, i.Y, i.DR, i.R_DR, "UAV: ", i.EB, i.PRB, i.CQI, i.SINR, i.PRX, i.V, i.M,
-          i.ES, i.EBC, i.Int)
+          i.ES, i.EBC, i.Int,"perdas",i.PRX, "SINR",i.SINR, "Data Rate:",i.DR/ 10**6,"Mbps")
+    if i.SINR:
+        nub=nub+1
+        mediaSIN=mediaSIN+i.SINR
+        mediapath = mediapath + i.PRX
+
 print("")
 print("Estatística da proposta")
 print('ON =', on)
 print('OFF =', s - on)
 
+print('MediaSIN =', mediaSIN/nub)
+print('MediaPATH =', mediapath/nub)
 for x in alluav:
-    print("Drone:", x.ID, "MAX_USER: ", x.MAX_U, "PRB: ", x.PRB_F, "cob: ", x.Cob, "m", "Alt: ", x.H, "m")
+    print("Drone:", x.ID, "MAX_USER: ", x.MAX_U, "PRB: ", x.PRB_F, "cob: ", x.Cob, "m", "Alt: ", x.H, "RecBlocks dipo",x.PRB_F)
+import math
+# Definindo as variáveis
+snr_dB = 30  # SNR em dB quanto maior melhor a relacao sinal ruido
+largura_banda_MHz = 40
+largura_banda_prb_Hz = largura_banda_MHz * 10**6
+#largura_banda_prb_Hz = 100 * 180 * 10**3  # Largura de banda por PRB em Hz (100 PRBs)
+# Função para calcular a taxa máxima da rede em bps
+def calcular_taxa_maxima(snr_dB, largura_banda_prb_Hz):
+    snr_linear = 10 ** (snr_dB / 10)  # Convertendo SNR para escala linear
+    taxa_maxima_bps = largura_banda_prb_Hz * math.log2(1 + snr_linear)  # Fórmula de Shannon
+    return taxa_maxima_bps
+
+# Calculando a taxa máxima da rede
+taxa_maxima_rede_bps = calcular_taxa_maxima(snr_dB, largura_banda_prb_Hz)
+taxa_maxima_rede_mbps = taxa_maxima_rede_bps / 10**6
+print("Taxa máxima da rede:", taxa_maxima_rede_mbps, "Mbps")
 
 
 # Plot todos os usuarios
